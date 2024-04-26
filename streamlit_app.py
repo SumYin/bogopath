38d2c0e7703343773acec73453e00a8f80037d53
SumYin <80163389+SumYin@users.noreply.github.com>
2024-04-26 20:05:44 +0000

    Sliders

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import math
 import graphviz
 
 st.write("""
@@ -7,11 +8,23 @@ st.write("""
 This is a demo of the bogosort algorithm where you can enter your own custom graph and the algorithm will try to solve it.
 """)
 
-graph=[("a", "b"), ("b", "c")]
+edges = st.slider('how many edges do you want?', 1, 10, 1)
+    
+cpaths = st.slider('how many possible simple paths will it have?', 0, edges, 1)
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+graph=[("a", "b")] * cpaths + [("a", "c")] * (edges - cpaths)
+
+df = pd.DataFrame(graph)
 
 df
+
+f = graphviz.Graph('finite_state_machine', filename='fsm.gv')
+f.attr(rankdir='LR', size='8,5')
+
+f.attr('node', shape='circle')
+f.attr(bgcolor="transparent")
+
+for i in graph:
+    f.edge(i[0], i[1])
+
+st.graphviz_chart(f)
